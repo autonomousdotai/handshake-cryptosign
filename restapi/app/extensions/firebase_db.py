fad02905f1e03ccbc8ef1228fdce71ef30608d63
Bakunawa <trong@autonomous.nyc>
2018-06-05 21:17:08 +0700

    Update firebase database

--- restapi/app/extensions/firebase_db.py
+++ restapi/app/extensions/firebase_db.py
@@ -5,7 +5,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 config = {
 	"apiKey": "",
 	"authDomain": "",
-	"databaseURL": "https://handshake-205007.firebaseio.com/",
+	"databaseURL": "",
 	"storageBucket": "",
 	"serviceAccount": dir_path + "/handshake-205007.json"
 }
@@ -14,10 +14,12 @@ class FirebaseDatabase(object):
 		super(FirebaseDatabase, self).__init__()
 		if app:
 			self.app = app
+			config['databaseURL'] = app.config['FIREBASE_DATABASE_URL']
 			self.firebase = pyrebase.initialize_app(config)
 			
 	def init_app(self, app):
 		self.app = app
+		config['databaseURL'] = app.config['FIREBASE_DATABASE_URL']
 		self.firebase = pyrebase.initialize_app(config)
 
 	def push_data(self, data, user_id):

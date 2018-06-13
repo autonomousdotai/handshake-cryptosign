a74ffe69cc59b58c757ff8ca85b21667a96ea03b
Sharlie <khanh.p@autonomous.nyc>
2018-06-13 16:36:17 +0700

    remove outcome id

--- event/libs/handShakeData.js
+++ event/libs/handShakeData.js
@@ -1,6 +1,5 @@
 module.exports = [
   {
-    "outcome_id": 173,
     "name": "Russia vs Saudi Arabia",
     "extra_data": "{\"event_name\":\"Russia vs Saudi Arabia\",\"event_predict\":\"Russia wins\"}",
     "outcomes": [
@@ -15,7 +14,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 174,
     "name": "Egypt vs Uruguay",
     "extra_data": "{\"event_name\":\"Egypt vs Uruguay\",\"event_predict\":\"Egypt wins\"}",
     "outcomes": [
@@ -30,7 +28,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 175,
     "name": "Morocco vs Iran",
     "extra_data": "{\"event_name\":\"Morocco vs Iran\",\"event_predict\":\"Morocco wins\"}",
     "outcomes": [
@@ -45,7 +42,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 176,
     "name": "Portugal vs Spain",
     "extra_data": "{\"event_name\":\"Portugal vs Spain\",\"event_predict\":\"Portugal wins\"}",
     "outcomes": [
@@ -60,7 +56,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 177,
     "name": "France vs Australia",
     "extra_data": "{\"event_name\":\"France vs Australia\",\"event_predict\":\"France wins\"}",
     "outcomes": [
@@ -75,7 +70,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 178,
     "name": "Argentina vs Iceland",
     "extra_data": "{\"event_name\":\"Argentina vs Iceland\",\"event_predict\":\"Argentina wins\"}",
     "outcomes": [
@@ -90,7 +84,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 179,
     "name": "Peru vs Denmark",
     "extra_data": "{\"event_name\":\"Peru vs Denmark\",\"event_predict\":\"Peru wins\"}",
     "outcomes": [
@@ -105,7 +98,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 180,
     "name": "Croatia vs Nigeria",
     "extra_data": "{\"event_name\":\"Croatia vs Nigeria\",\"event_predict\":\"Croatia wins\"}",
     "outcomes": [
@@ -120,7 +112,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 181,
     "name": "Costa Rica vs Serbia",
     "extra_data": "{\"event_name\":\"Costa Rica vs Serbia\",\"event_predict\":\"Costa Rica wins\"}",
     "outcomes": [
@@ -135,7 +126,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 182,
     "name": "Germany vs Mexico",
     "extra_data": "{\"event_name\":\"Germany vs Mexico\",\"event_predict\":\"Germany wins\"}",
     "outcomes": [
@@ -150,7 +140,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 183,
     "name": "Brazil vs Switzerland",
     "extra_data": "{\"event_name\":\"Brazil vs Switzerland\",\"event_predict\":\"Brazil wins\"}",
     "outcomes": [
@@ -165,7 +154,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 185,
     "name": "Belgium vs Panama",
     "extra_data": "{\"event_name\":\"Belgium vs Panama\",\"event_predict\":\"Belgium wins\"}",
     "outcomes": [
@@ -180,7 +168,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 184,
     "name": "Sweden vs Korea Republic",
     "extra_data": "{\"event_name\":\"Sweden vs Korea Republic\",\"event_predict\":\"Sweden wins\"}",
     "outcomes": [
@@ -195,7 +182,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 186,
     "name": "Tunisia vs England",
     "extra_data": "{\"event_name\":\"Tunisia vs England\",\"event_predict\":\"Tunisia wins\"}",
     "outcomes": [
@@ -210,7 +196,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 187,
     "name": "Colombia vs Japan",
     "extra_data": "{\"event_name\":\"Colombia vs Japan\",\"event_predict\":\"Colombia wins\"}",
     "outcomes": [
@@ -225,7 +210,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 188,
     "name": "Poland vs Senegal",
     "extra_data": "{\"event_name\":\"Poland vs Senegal\",\"event_predict\":\"Poland wins\"}",
     "outcomes": [
@@ -240,7 +224,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 189,
     "name": "Russia vs Egypt",
     "extra_data": "{\"event_name\":\"Russia vs Egypt\",\"event_predict\":\"Russia wins\"}",
     "outcomes": [
@@ -255,7 +238,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 190,
     "name": "Portugal vs Morocco",
     "extra_data": "{\"event_name\":\"Portugal vs Morocco\",\"event_predict\":\"Portugal wins\"}",
     "outcomes": [
@@ -270,7 +252,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 191,
     "name": "Uruguay vs Saudi Arabia",
     "extra_data": "{\"event_name\":\"Uruguay vs Saudi Arabia\",\"event_predict\":\"Uruguay wins\"}",
     "outcomes": [
@@ -285,7 +266,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 192,
     "name": "Iran vs Spain",
     "extra_data": "{\"event_name\":\"Iran vs Spain\",\"event_predict\":\"Iran wins\"}",
     "outcomes": [
@@ -300,7 +280,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 193,
     "name": "Denmark vs Australia",
     "extra_data": "{\"event_name\":\"Denmark vs Australia\",\"event_predict\":\"Denmark wins\"}",
     "outcomes": [
@@ -315,7 +294,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 194,
     "name": "France vs Peru",
     "extra_data": "{\"event_name\":\"France vs Peru\",\"event_predict\":\"France wins\"}",
     "outcomes": [
@@ -330,7 +308,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 195,
     "name": "Argentina vs Croatia",
     "extra_data": "{\"event_name\":\"Argentina vs Croatia\",\"event_predict\":\"Argentina wins\"}",
     "outcomes": [
@@ -345,7 +322,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 196,
     "name": "Brazil vs Costa Rica",
     "extra_data": "{\"event_name\":\"Brazil vs Costa Rica\",\"event_predict\":\"Brazil wins\"}",
     "outcomes": [
@@ -360,7 +336,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 197,
     "name": "Nigeria vs Iceland",
     "extra_data": "{\"event_name\":\"Nigeria vs Iceland\",\"event_predict\":\"Nigeria wins\"}",
     "outcomes": [
@@ -375,7 +350,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 198,
     "name": "Serbia vs Switzerland",
     "extra_data": "{\"event_name\":\"Serbia vs Switzerland\",\"event_predict\":\"Serbia wins\"}",
     "outcomes": [
@@ -390,7 +364,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 199,
     "name": "Belgium vs Tunisia",
     "extra_data": "{\"event_name\":\"Belgium vs Tunisia\",\"event_predict\":\"Belgium wins\"}",
     "outcomes": [
@@ -405,7 +378,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 200,
     "name": "Korea Republic vs Mexico",
     "extra_data": "{\"event_name\":\"Korea Republic vs Mexico\",\"event_predict\":\"Korea Republic wins\"}",
     "outcomes": [
@@ -420,7 +392,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 202,
     "name": "England vs Panama",
     "extra_data": "{\"event_name\":\"England vs Panama\",\"event_predict\":\"England wins\"}",
     "outcomes": [
@@ -435,7 +406,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 203,
     "name": "Japan vs Senegal",
     "extra_data": "{\"event_name\":\"Japan vs Senegal\",\"event_predict\":\"Japan wins\"}",
     "outcomes": [
@@ -450,7 +420,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 204,
     "name": "Poland vs Colombia",
     "extra_data": "{\"event_name\":\"Poland vs Colombia\",\"event_predict\":\"Poland wins\"}",
     "outcomes": [
@@ -465,7 +434,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 205,
     "name": "Saudi Arabia vs Egypt",
     "extra_data": "{\"event_name\":\"Saudi Arabia vs Egypt\",\"event_predict\":\"Saudi Arabia wins\"}",
     "outcomes": [
@@ -480,7 +448,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 201,
     "name": "Germany vs Sweden",
     "extra_data": "{\"event_name\":\"Germany vs Sweden\",\"event_predict\":\"Germany wins\"}",
     "outcomes": [
@@ -495,7 +462,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 206,
     "name": "Uruguay vs Russia",
     "extra_data": "{\"event_name\":\"Uruguay vs Russia\",\"event_predict\":\"Uruguay wins\"}",
     "outcomes": [
@@ -510,7 +476,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 208,
     "name": "Spain vs Morocco",
     "extra_data": "{\"event_name\":\"Spain vs Morocco\",\"event_predict\":\"Spain wins\"}",
     "outcomes": [
@@ -525,7 +490,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 207,
     "name": "Iran vs Portugal",
     "extra_data": "{\"event_name\":\"Iran vs Portugal\",\"event_predict\":\"Iran wins\"}",
     "outcomes": [
@@ -540,7 +504,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 209,
     "name": "Denmark vs France",
     "extra_data": "{\"event_name\":\"Denmark vs France\",\"event_predict\":\"Denmark wins\"}",
     "outcomes": [
@@ -555,7 +518,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 210,
     "name": "Australia vs Peru",
     "extra_data": "{\"event_name\":\"Australia vs Peru\",\"event_predict\":\"Australia wins\"}",
     "outcomes": [
@@ -570,7 +532,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 211,
     "name": "Nigeria vs Argentina",
     "extra_data": "{\"event_name\":\"Nigeria vs Argentina\",\"event_predict\":\"Nigeria wins\"}",
     "outcomes": [
@@ -585,7 +546,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 212,
     "name": "Iceland vs Croatia",
     "extra_data": "{\"event_name\":\"Iceland vs Croatia\",\"event_predict\":\"Iceland wins\"}",
     "outcomes": [
@@ -600,7 +560,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 213,
     "name": "Mexico vs Sweden",
     "extra_data": "{\"event_name\":\"Mexico vs Sweden\",\"event_predict\":\"Mexico wins\"}",
     "outcomes": [
@@ -615,7 +574,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 214,
     "name": "Korea Republic vs Germany",
     "extra_data": "{\"event_name\":\"Korea Republic vs Germany\",\"event_predict\":\"Korea Republic wins\"}",
     "outcomes": [
@@ -630,7 +588,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 215,
     "name": "Serbia vs Brazil",
     "extra_data": "{\"event_name\":\"Serbia vs Brazil\",\"event_predict\":\"Serbia wins\"}",
     "outcomes": [
@@ -645,7 +602,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 216,
     "name": "Switzerland vs Costa Rica",
     "extra_data": "{\"event_name\":\"Switzerland vs Costa Rica\",\"event_predict\":\"Switzerland wins\"}",
     "outcomes": [
@@ -660,7 +616,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 217,
     "name": "Japan vs Poland",
     "extra_data": "{\"event_name\":\"Japan vs Poland\",\"event_predict\":\"Japan wins\"}",
     "outcomes": [
@@ -675,7 +630,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 218,
     "name": "Senegal vs Colombia",
     "extra_data": "{\"event_name\":\"Senegal vs Colombia\",\"event_predict\":\"Senegal wins\"}",
     "outcomes": [
@@ -690,7 +644,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 219,
     "name": "Panama vs Tunisia",
     "extra_data": "{\"event_name\":\"Panama vs Tunisia\",\"event_predict\":\"Panama wins\"}",
     "outcomes": [
@@ -705,7 +658,6 @@ module.exports = [
     ]
   },
   {
-    "outcome_id": 220,
     "name": "England vs Belgium",
     "extra_data": "{\"event_name\":\"England vs Belgium\",\"event_predict\":\"England wins\"}",
     "outcomes": [

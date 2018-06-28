a57bbf110b8f949598d844deb222bdaec5dfe2fc
Bakunawa <trong@autonomous.nyc>
2018-06-28 10:29:54 +0700

    Refactor event

--- restapi/app/routes/event.py
+++ restapi/app/routes/event.py
@@ -29,58 +29,30 @@ def event():
 		
 		handshakes = []
 		shakers = []
-
 		if status == 1:
 			event_name = data['eventName']
 			inputs = data['inputs']
-
-			# parse inputs
-			offchain = inputs['offchain']
-			hid = int(inputs['hid'])
-
-			response_json = []
-			if '__createMarket' in event_name:
-				offchain = int(offchain.replace('createMarket', ''))
-				outcome = Outcome.find_outcome_by_id(offchain)
-				if outcome is None:
-					return response_error(MESSAGE.OUTCOME_INVALID, CODE.OUTCOME_INVALID)
-				else:
-					outcome.hid = hid
-					db.session.flush()
-
-					response_json.append(outcome.to_json())
-
-			else:
-				outcome = Outcome.find_outcome_by_hid(hid)
-				if outcome is None:
-					return response_error(MESSAGE.OUTCOME_INVALID, CODE.OUTCOME_INVALID)
-				
-				handshakes, shakers = handshake_bl.save_handshake_for_event(event_name, offchain, outcome)
-
-			db.session.commit()
-
+			handshakes, shakers = handshake_bl.save_handshake_for_event(event_name, inputs)
 		else:
 			# TODO:
 			method = data.get('methodName', '')
 			inputs = data['inputs']
 
-
+		db.session.commit()
 		# update feed
 		if handshakes is not None:
 			for handshake in handshakes:
-				response_json.append(handshake.to_json())
 				update_feed.delay(handshake.id)
 				if '__init' in event_name:
 					add_shuriken(handshake.user_id)
 
 		if shakers is not None:
 			for shaker in shakers:
-				response_json.append(shaker.to_json())
 				update_feed.delay(shaker.handshake_id)
 				if '__shake' in event_name:
 					add_shuriken(shaker.shaker_id)
 
-		return response_ok(response_json)
+		return response_ok()
 	except Exception, ex:
 		db.session.rollback()
 		return response_error(ex.message)

c90c2549918109816c50ad4a94e9c84151b4e83b
Bakunawa <trong@autonomous.nyc>
2018-07-01 21:49:35 +0700

    Remove redundant code

--- event-old-25-June/cron/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    bettingCron: require('./cronBetting')
-};
--- event-old-25-June/index.js
+++
@@ -1,7 +0,0 @@
-
-const configs = require('./configs');
-const cron = require('./cron');
-
-setTimeout( () => {
-    cron.bettingCron.runBettingCron();
-}, 1)

aea8bce75ea4fb3b41f2d638394d98ef37aeba9a
Sharlie <khanh.p@autonomous.nyc>
2018-07-04 14:33:07 +0700

    fix bug

--- nodebl/configs/web3.js
+++ nodebl/configs/web3.js
@@ -2,7 +2,7 @@
 const configs = require('../configs');
 const Web3 = require('web3');
 
-const currentNonce = 0;
+let currentNonce = 0;
 const web3Provider = new Web3(new Web3.providers.HttpProvider(configs.network[configs.network_id].blockchainNetwork));
 
 const getWeb3 = () => {

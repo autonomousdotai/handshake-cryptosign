5615d58878b0e9e0da634deed1fe1e94490c387e
0xBakunawa <trong@autonomous.nyc>
2018-07-17 16:10:59 +0700

    DEBUG

--- restapi/app/bl/task.py
+++ restapi/app/bl/task.py
@@ -9,6 +9,8 @@ import json
 
 def is_able_to_create_new_task(outcome_id):
 	tasks = db.session.query(Task).filter(and_(Task.action == 'INIT', (Task.date_created + timedelta(seconds=300)) > datetime.now())).order_by(Task.date_created.desc()).all()
+	print '----- WTF -----'
+	print '{} {}'.format(datetime.now(), tasks)
 	if tasks is not None:
 		for task in tasks:
 			try:
@@ -19,7 +21,7 @@ def is_able_to_create_new_task(outcome_id):
 					if n - ds > 300:
 						return True
 
-					print '----- WTF -----'
+					print '----- WTF 123 -----'
 					print '{} {}'.format(n, ds)
 					return False
 			except Exception as ex:

e72fed14d0dc17d7d315bc07cbc1e0a8965c4f6c
Sharlie <khanh.p@autonomous.nyc>
2018-07-18 13:49:12 +0700

    debug

--- event/models/base.go
+++ event/models/base.go
@@ -12,8 +12,8 @@ type BaseModel struct {
 }
 
 func (m *BaseModel) BeforeCreate() (err error) {
-	m.DateCreated = time.UTC()
-	m.DateModified = time.UTC()
+	m.DateCreated = time.Now().UTC()
+	m.DateModified = time.Now().UTC()
 	return
 }
 

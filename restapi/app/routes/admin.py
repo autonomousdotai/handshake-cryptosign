9fb262bfdbeeff98e8412a1a9d3dea4be5466fee
Sharlie <khanh.p@autonomous.nyc>
2018-08-16 16:28:12 +0700

    fix bug

--- restapi/app/routes/admin.py
+++ restapi/app/routes/admin.py
@@ -216,7 +216,7 @@ def report(match_id):
 				task = Task(
 					task_type=CONST.TASK_TYPE['REAL_BET'],
 					data=json.dumps(report),
-					action=CONST.TASK_ACTION['REPORT' if disputed else 'RESOLVE'],
+					action=CONST.TASK_ACTION['RESOLVE' if disputed else 'REPORT'],
 					status=-1,
 					contract_address= contract.contract_address,
 					contract_json= contract.contract_json

ebb55ee1929636b3425b6256589ed25928237ab0
0xBakunawa <trong@autonomous.nyc>
2018-08-20 19:56:42 +0700

    Fix send mail

--- event/daos/outcome.go
+++ event/daos/outcome.go
@@ -12,7 +12,7 @@ type OutcomeDAO struct{}
 // GetAllOutcomesWithNoResult : matchID
 func (m OutcomeDAO) GetAllOutcomesWithNoResult(matchID int) ([]models.Outcome, error) {
 	outcomes := []models.Outcome{}
-	err := models.Database().Where("outcome.hid >= 0 and outcome.result = -1 and outcome.match_id = ?", matchID).Find(&outcomes).Error
+	err := models.Database().Where("outcome.hid >= 0 and outcome.result = -1 and outcome.match_id = ?", matchID).Or("outcome.hid >= 0 and outcome.result = -3 and outcome.match_id = ?", matchID).Find(&outcomes).Error
 	if err != nil {
 		fmt.Println(err)
 		return nil, err

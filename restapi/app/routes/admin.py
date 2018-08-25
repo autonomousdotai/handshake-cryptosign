6529b19b004308572275d24dbb3a259e0fe2d320
0xBakunawa <trong@autonomous.nyc>
2018-08-25 13:25:36 +0700

    DEBUG

--- restapi/app/routes/admin.py
+++ restapi/app/routes/admin.py
@@ -186,6 +186,7 @@ def report_match(match_id):
 		if data is None:
 			return response_error(MESSAGE.INVALID_DATA, CODE.INVALID_DATA)
 
+		log.debug("DEBUG --> {}".format(data))
 		match = db.session.query(Match).filter(Match.date < seconds, Match.id == match_id).first()
 		if match is not None:
 			result = data['result']

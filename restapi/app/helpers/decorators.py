98ba839f279435d620a8a9fbb63a5bbd12398b73
Sharlie <khanh.p@autonomous.nyc>
2018-10-17 17:51:34 +0700

    fix bug

--- restapi/app/helpers/decorators.py
+++ restapi/app/helpers/decorators.py
@@ -89,8 +89,7 @@ def service_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         remote = request.remote_addr
-
-        if "10." not in remote or remote not in white_ips:
+        if "10." not in remote and remote not in white_ips:
             return response_error("Access deny!")
 
         return f(*args, **kwargs)

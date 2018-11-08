1a8288cabc77623f80cbb33d3c784487a05caa95
0xBakunawa <trong@autonomous.nyc>
2018-11-08 13:35:53 +0700

    update code

--- event/services/recombee.go
+++ event/services/recombee.go
@@ -13,31 +13,17 @@ import (
 )
 
 // RecombeeService : struct
-type RecombeeService struct{}
-
-// Item : struct
-type Item struct {
-	MatchID    int
-	Name       string
-	Tags       []string
-	SourceID   int
-	CategoryID int
+type RecombeeService struct {
+	dbName     string
+	dbKey      string
+	dbEndpoint string
 }
 
 // ImportMatches : matches
-func (s RecombeeService) ImportMatches(matches []models.Match) (err error) {
-	conf := config.GetConfig()
+func (recombee RecombeeService) ImportMatches(matches []models.Match) (err error) {
 
-	recombeeData := conf.GetStringMapString("recombee")
-	var recombeeDB, recombeeKey, recombeeHost string
-	for key, value := range recombeeData {
-		if key == "db_name" {
-			recombeeDB = value
-		} else if key == "key" {
-			recombeeKey = value
-		} else if key == "endpoint" {
-			recombeeHost = value
-		}
+	if recombee.dbName == "" || recombee.dbKey == "" || recombee.dbEndpoint == "" {
+		recombee.loadConfigs()
 	}
 
 	for index := 0; index < len(matches); index++ {
@@ -56,10 +42,10 @@ func (s RecombeeService) ImportMatches(matches []models.Match) (err error) {
 
 		// Request to Recombee
 		hmacTimestamp := time.Now().UnixNano() / int64(time.Second) // time.Millisecond
-		url := fmt.Sprintf("/%s/items/%d?%s", recombeeDB, m.ID, fmt.Sprintf("%s%d", "hmac_timestamp=", hmacTimestamp))
-		hmacSign := utils.ComputeSHA1(url, recombeeKey)
+		url := fmt.Sprintf("/%s/items/%d?%s", recombee.dbName, m.ID, fmt.Sprintf("%s%d", "hmac_timestamp=", hmacTimestamp))
+		hmacSign := utils.ComputeSHA1(url, recombee.dbKey)
 		url = fmt.Sprintf("%s&hmac_sign=%s", url, hmacSign)
-		endpoint := fmt.Sprintf("%s%s", recombeeHost, url)
+		endpoint := fmt.Sprintf("%s%s", recombee.dbEndpoint, url)
 
 		request, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonValue))
 		request.Header.Set("Content-Type", "application/json")
@@ -73,4 +59,19 @@ func (s RecombeeService) ImportMatches(matches []models.Match) (err error) {
 		}
 	}
 	return nil
+}
+
+func (recombee *RecombeeService) loadConfigs() {
+	conf := config.GetConfig()
+
+	recombeeData := conf.GetStringMapString("recombee")
+	for key, value := range recombeeData {
+		if key == "db_name" {
+			recombee.dbName = value
+		} else if key == "key" {
+			recombee.dbKey = value
+		} else if key == "endpoint" {
+			recombee.dbEndpoint = value
+		}
+	}
 }

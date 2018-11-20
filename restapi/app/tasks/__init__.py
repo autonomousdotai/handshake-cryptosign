d8c75a9e960f3f4ca347460d0d517ddd3207916b
Sharlie <khanh.p@autonomous.nyc>
2018-11-20 09:46:39 +0700

    celery crop file

--- restapi/app/tasks/__init__.py
+++ restapi/app/tasks/__init__.py
@@ -417,6 +417,7 @@ def update_status_feed(_id, status):
 @celery.task()
 def upload_file_google_storage(match_id, image_name, saved_path):
 	try:
+		saved_path = storage_bl.handle_crop_image(saved_path)
 		match = Match.find_match_by_id(match_id)
 		if match is None:
 			return False

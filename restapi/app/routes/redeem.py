ef868c8917ba1a557a14412151809833a1909f92
0xBakunawa <trong@autonomous.nyc>
2018-11-20 18:14:22 +0700

    Fix check redeem

--- restapi/app/routes/redeem.py
+++ restapi/app/routes/redeem.py
@@ -55,7 +55,7 @@ def check_redeem():
 		response = {
 			"amount": CONST.CRYPTOSIGN_FREE_BET_AMOUNT
 		}
-		r = db.session.query(Redeem).filter(Redeem.code==redeem, Redeem.reserved_id==uid).all()
+		r = db.session.query(Redeem).filter(Redeem.code==redeem, Redeem.reserved_id==uid, Redeem.used_user==0).all()
 		if r is not None and len(r) > 0:
 			return response_ok(response)
 

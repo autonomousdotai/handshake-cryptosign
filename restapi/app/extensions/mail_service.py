a253b2ec5db02f34d06e243d6677a4291104d6f3
0xBakunawa <trong@autonomous.nyc>
2018-11-23 14:23:15 +0700

    Fix mail service

--- restapi/app/extensions/mail_service.py
+++ restapi/app/extensions/mail_service.py
@@ -17,7 +17,7 @@ class MailService(object):
 			endpoint = '{}'.format(self.app.config['MAIL_SERVICE'])
 			multipart_form_data = MultipartEncoder(
 				fields= {
-					'body': _body,
+					'body': _body.encode(encoding='UTF-8', errors='strict'),
 					'subject': _subject,
 					'to[]': _to_arr,
 					'from': _from

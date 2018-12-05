b68821d0c4e7b6cc7bd68609f99bc487ea3f77de
Sharlie <khanh.p@autonomous.nyc>
2018-12-05 17:10:41 +0700

    user alias

--- restapi/app/routes/hook.py
+++ restapi/app/routes/hook.py
@@ -29,15 +29,29 @@ def user_hook():
 		user_id = data.get('user_id', None)
 		email = data.get('email', None)
 		meta_data = data.get('meta_data', None)
+		user_name = data.get('user_name', None)
+		name = data.get('name', None)
 
 		if type_change == "Update":
 			user = User.find_user_with_id(user_id)
 			
-			if user is not None and user.email != email and email is not None and email != "":
-				print "Update email: {}".format(email)
-				user.email = email
-				db.session.commit()
+			if user is not None:
+				if user.email != email and email is not None and email != "":
+					print "Update email: {}".format(email)
+					user.email = email
+					db.session.flush()
 
+				if user.name != name and name is not None and name != "":
+					print "Update name: {}".format(name)
+					user.name = name
+					db.session.flush()
+
+				if user.user_name != user_name and user_name is not None and user_name != "":
+					print "Update user_name: {}".format(user_name)
+					user.user_name = user_name
+					db.session.flush()
+
+		db.session.commit()
 		return response_ok()
 
 	except Exception, ex:

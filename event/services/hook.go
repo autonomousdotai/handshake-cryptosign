f9bb9fcc1ec19b53040db38965e8da004aaa7d22
Sharlie <khanh.p@autonomous.nyc>
2019-02-18 11:48:55 +0700

    user token hook

--- event/services/hook.go
+++ event/services/hook.go
@@ -73,3 +73,40 @@ func (h HookService) PostSlack(msg string) {
 	resp, _ := client.Do(req)
 	fmt.Println(resp.Status)
 }
+
+// Event : send UserToken data
+func (h HookService) UserTokenEvent(jsonData map[string]interface{}) error {
+	conf := config.GetConfig()
+
+	endpoint := conf.GetString("hookEndpoint")
+	endpoint = fmt.Sprintf("%s/user-token", endpoint)
+	jsonValue, _ := json.Marshal(jsonData)
+
+	request, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonValue))
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	b, _ := ioutil.ReadAll(response.Body)
+
+	var data map[string]interface{}
+	json.Unmarshal(b, &data)
+
+	status, ok := data["status"]
+	message, hasMessage := data["message"]
+
+	if ok && status.(float64) > 0 {
+		return nil
+	} else {
+		errStr := "Unknown"
+		if hasMessage {
+			errStr = message.(string)
+		}
+		return errors.New(errStr)
+	}
+}
